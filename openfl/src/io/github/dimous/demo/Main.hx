ef04278a38b5e4fca89600711e030d06b43f8029
dimous <kasimowsky@gmail.com>
2019-03-12 17:17:06 +1000

    #

--- openfl/src/io/github/dimous/demo/Main.hx
+++ openfl/src/io/github/dimous/demo/Main.hx
@@ -18,36 +18,36 @@ class Main extends Sprite {
 	public function new() {
 		super();
 
-        LogTransmitter.setActive(true);
+		LogTransmitter.setActive(true);
 
-        LogTransmitter.onMessage = LogAppender.accept;		
-		
+		LogTransmitter.onMessage = LogAppender.accept;		
+
 		final mediaio = new MediaIO(), timer = new Timer(1000 / 60), bitmapdata = new BitmapData(196, 312, true, 0x00000000), bitmap = new Bitmap(bitmapdata), format = mediaio.listInputFormats().filter(format -> DSHOW == format.getName()).pop();
-		
+
 		bitmap.x = 50;
 		bitmap.y = 50;
-		
+
 		this.addChild(bitmap);
-		
-        mediaio.onBeforePixelData = bitmapdata.lock;
-        mediaio.onPixelData = bitmapdata.setPixel32;
-        mediaio.onAfterPixelData = bitmapdata.unlock.bind(null);
-        mediaio.onStreamEnd = function () trace("stream ended", timer.currentCount);
-		
-        timer.addEventListener(TimerEvent.TIMER, function (_) mediaio.read());
-		
+
+		mediaio.onBeforePixelData = bitmapdata.lock;
+		mediaio.onPixelData = bitmapdata.setPixel32;
+		mediaio.onAfterPixelData = bitmapdata.unlock.bind(null);
+		mediaio.onStreamEnd = function () trace("stream ended", timer.currentCount);
+
+		timer.addEventListener(TimerEvent.TIMER, function (_) mediaio.read());
+
 		// test 1
-		// mediaio.open(196, 312, null, null, "./assets/video/sample.webm"/* you can even pass url here, for example -- http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4 */);
-		// timer.delay = 1000 / mediaio.getFrameRate();
-		
+		mediaio.open(196, 312, null, null, "./assets/video/sample.webm"/* you can even pass url here, for example -- http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4 */);
+		timer.delay = 1000 / mediaio.getFrameRate();
+
 		// test 2: stream webcam output (DSHOW/VFWCAP on Windows, AVFOUNDATION on iOS, VIDEO4LINUX2 on Linux, ANDROID_CAMERA on Android)
 		// mediaio.open(196, 312, null, format, "video=" + mediaio.listInputDevices(format).pop().getId());
 		// timer.delay = 1000 / mediaio.getFrameRate();
-				
+
 		// test 3
-		mediaio.open(0, 0, null, null, "./assets/sound/sample.mp3");
-		timer.delay = 1000 / mediaio.getFrameRate(AUDIO);
-		
-        timer.start();
+		// mediaio.open(0, 0, null, null, "./assets/sound/sample.mp3");
+		// timer.delay = 1000 / mediaio.getFrameRate(AUDIO);
+
+		timer.start();
 	}
 }
